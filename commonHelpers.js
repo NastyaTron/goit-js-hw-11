6a8b901b33e8321ca3142b2218a22d68d77fd00c
NastyaTron <151436238+NastyaTron@users.noreply.github.com>
2024-05-11 15:17:13 +0000

    Deploying to gh-pages from @ NastyaTron/goit-js-hw-11@038f44e1c57253c193e4f301940ba197356b5d9b 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,25 @@
-(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const s of document.querySelectorAll('link[rel="modulepreload"]'))n(s);new MutationObserver(s=>{for(const t of s)if(t.type==="childList")for(const a of t.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&n(a)}).observe(document,{childList:!0,subtree:!0});function o(s){const t={};return s.integrity&&(t.integrity=s.integrity),s.referrerPolicy&&(t.referrerPolicy=s.referrerPolicy),s.crossOrigin==="use-credentials"?t.credentials="include":s.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function n(s){if(s.ep)return;s.ep=!0;const t=o(s);fetch(s.href,t)}})();const c=r=>r.map(e=>{`${e.webformatURL}${e.tags}${e.likes}${e.views}${e.comments}${e.downloads}`}).join(""),l="https://pixabay.com/api/",i="43826847-f350fa9fd32410ec3fefedfc5",f=(r="flowers")=>{const e=new URLSearchParams({key:i,q:r,image_type:"photo",orientation:"horizontal",safesearch:"true"});return fetch(`${l}?${e}`).then(o=>{if(!o.ok)throw new Error("Error!");return o.json()})};f().then(r=>{const e=c(r);console.log(e)}).catch(r=>console.log(r));
+(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))a(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const n of r.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&a(n)}).observe(document,{childList:!0,subtree:!0});function o(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerPolicy&&(r.referrerPolicy=t.referrerPolicy),t.crossOrigin==="use-credentials"?r.credentials="include":t.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function a(t){if(t.ep)return;t.ep=!0;const r=o(t);fetch(t.href,r)}})();const c=s=>s.map(e=>`<div class="js-section-images">
+      
+        <img
+          src="${e.webformatURL}"
+          alt="${e.tags}"
+          class="js-gallery-img"
+        />
+        <div class="js-group-text">
+          <small class="js-text"
+            >Likes <span class="js-span-text">${e.likes}</span></small
+          >
+          <small class="js-text"
+            >Views <span class="js-span-text">${e.views}</span></small
+          >
+          <small class="js-text"
+            >Comments <span class="js-span-text">${e.comments}</span></small
+          >
+          <small class="js-text"
+            >Downloads
+            <span class="js-span-text">${e.downloads}</span></small
+          >
+        </div>
+      
+    </div>`).join(""),i="https://pixabay.com/api/",u="43826847-f350fa9fd32410ec3fefedfc5",f=(s="flowers")=>{const e=new URLSearchParams({key:u,q:s,image_type:"photo",orientation:"horizontal",safesearch:"true"});return fetch(`${i}?${e}`).then(o=>{if(!o.ok)throw new Error("Error!");return o.json()})},l=document.querySelector(".gallery"),m=document.querySelector(".form-field");m.addEventListener("submit",d);function d(s){s.preventDefault();const e=s.target.elements.searchKeyword.value.trim();console.log(e),e===""&&(l.innerHTML="",s.target.reset())}f().then(s=>{const e=c(s.hits);l.innerHTML=e}).catch(s=>console.log(s));
 //# sourceMappingURL=commonHelpers.js.map
